698919a2a1fb34904e7308156f2d64c1df9651a6
T.M Siddiquee <siddiquee09@gmail.com>
2022-12-20 01:23:53 +0600

    final backup

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,27 +16,29 @@ const Home = () => {
     loop: 5,
   });
   return (
-    <div className="container d-flex mt-2 justify-content-center align-items-center">
-      <div className="mt-2 w-75">
-        <lottie-player
-          src="https://assets9.lottiefiles.com/packages/lf20_9ungcrzx.json"
-          background="transparent"
-          speed="1"
-          style={{ width: "300px", height: "300px" }}
-          loop
-          autoplay
-        ></lottie-player>
-      </div>
-      <div className="">
-        <h2 className="fs-1 mt-5">
-          Learn in- demand skills and{" "}
-          <span className="text-success">{text}</span>
-        </h2>
-        <Button className="ms-3">
-          <Link to="/courses/:id" className="text-decoration-none text-white">
-            Start Learning
-          </Link>
-        </Button>
+    <div>
+      <div className="container d-md-flex mt-2 align-items-center">
+        <div className="mt-2">
+          <lottie-player
+            src="https://assets9.lottiefiles.com/packages/lf20_9ungcrzx.json"
+            background="transparent"
+            speed="1"
+            style={{ width: "500px", height: "300px" }}
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
+        <div className="">
+          <h2 className="fs-1 mt-5">
+            Learn in- demand skills and{" "}
+            <span className="text-success">{text}</span>
+          </h2>
+          <Button className="ms-3">
+            <Link to="/courses/:id" className="text-decoration-none text-white">
+              Start Learning
+            </Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
